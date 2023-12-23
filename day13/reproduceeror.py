984d1d44f295dfbbaf99eb3397404480182ea238
daniel <danielkebede381@gmail.com>
2023-12-23 12:46:47 -0500

    day 13 completed

---
+++ day13/reproduceeror.py
@@ -0,0 +1,12 @@
+# import random
+
+# dice_imgs=["1","2","3","4","5","6"]
+# dice_num=random.randint(0,5)
+# print(dice_imgs[dice_num])
+
+pages=0
+word_per_page=0
+pages=int(input("How many words"))
+word_per_page=int(input("How many words"))
+total_words=pages*word_per_page
+print(total_words)
